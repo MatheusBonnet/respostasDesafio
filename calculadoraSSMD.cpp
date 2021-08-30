f70c478eb79fb090efb17d0fdbf512f4dccb2a5b
leourb44 <82837996+leourb44@users.noreply.github.com>
2021-08-30 19:53:42 -0300

    Add files via upload

--- calculadoraSSMD.cpp
+++ calculadoraSSMD.cpp
@@ -12,28 +12,57 @@ int main (){
 	cout << "Escolha uma das operacoes a baixo:\nSoma tecle 1\nSubtracao tecle 2\nMultiplicacao tecle 3\nDivisao tecle 4\n";
 	cin >> equacao;
 	
+	if (equacao > 4){
+		cout <<"Numero invalido, tente novamente";
+	}
+	
+	if (equacao ==  1){
+		
 	cout << "Entre com o primeiro numero: ";
 	cin >> numero_escolhido1;
+	
 	cout << "Entre com o segundo numero: ";
 	cin >> numero_escolhido2;
 	
-	if (equacao ==  1){
-		resultado = numero_escolhido1 + numero_escolhido2;
-		cout <<"A soma dos numeros digitados e igual a: ";
+	resultado = numero_escolhido1 + numero_escolhido2;
+	
+	cout <<"A soma dos numeros digitados e igual a: ";
 	}
 	
 	if (equacao ==  2){
-		resultado = numero_escolhido1 - numero_escolhido2;
-		cout <<"A subtracao dos numeros digitados e igual a: ";
+		
+	cout << "Entre com o primeiro numero: ";
+	cin >> numero_escolhido1;
+	
+	cout << "Entre com o segundo numero: ";
+	cin >> numero_escolhido2;
+		
+	resultado = numero_escolhido1 - numero_escolhido2;
+	cout <<"A subtracao dos numeros digitados e igual a: ";
 	}
 	if (equacao ==  3){
-		resultado = numero_escolhido1 * numero_escolhido2;
-		cout <<"A multiplicacao dos numeros digitados e igual a: ";
+		
+	cout << "Entre com o primeiro numero: ";
+	cin >> numero_escolhido1;
+	
+	cout << "Entre com o segundo numero: ";
+	cin >> numero_escolhido2;
+		
+	resultado = numero_escolhido1 * numero_escolhido2;
+	cout <<"A multiplicacao dos numeros digitados e igual a: ";
 	}
 	if (equacao ==  4){
-		resultado = numero_escolhido1 / numero_escolhido2;
-		cout <<"A divisao dos numeros digitados e igual a: " ;
+		
+	cout << "Entre com o primeiro numero: ";
+	cin >> numero_escolhido1;
+	
+	cout << "Entre com o segundo numero: ";
+	cin >> numero_escolhido2;
+		
+	resultado = numero_escolhido1 / numero_escolhido2;
+	cout <<"A divisao dos numeros digitados e igual a: " ;
 	}
+	
 	
 	if(resultado < 0){
 		resultado = resultado * -1;
