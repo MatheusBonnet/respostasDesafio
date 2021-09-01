db56dfbfdb5b071411322e174ea1bf5cf2f6a34c
leourb44 <82837996+leourb44@users.noreply.github.com>
2021-09-01 20:00:43 -0300

    Add files via upload

---
+++ nilton.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main (){
+	
+	float massa = 20.0;
+	float newton = 220.0;
+	float aceleracao = 0.0;
+	
+	aceleracao = newton/massa;
+	
+	cout << aceleracao << " m/s";
+	
+	return 0;
+	
+}
